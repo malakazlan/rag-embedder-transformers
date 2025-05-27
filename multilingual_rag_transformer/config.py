5fd153daae70487bd10cc1a98e8480277e066ed4
malakazlan <azlanmalik999@gmail.com>
2025-05-27 21:36:58 +0500

    llm integration

--- multilingual_rag_transformer/config.py
+++ multilingual_rag_transformer/config.py
@@ -1,4 +1,14 @@
+<<<<<<< HEAD
 PINECONE_API_KEY = "pcsk_4TJUMo_CTGUzsY4wXQBh7mn4bJHteQj832zUaJzywaD7KQCubtJAPbXVpb7S6A73Zttr82"
 PINECONE_ENV = "us-east1-gcp"
-PINECONE_INDEX_NAME = "distl-mbert-rag"
+PINECONE_INDEX_NAME = "medical"
+
+# Add your Google GenAI API key here
+GOOGLE_GENAI_API_KEY = "google PI HERE "
+GOOGLE_GENAI_MODEL = "models/gemini-2.0-flash-001"
+=======
+PINECONE_API_KEY = "your  pinecone api key "
+PINECONE_ENV = "pinrcone enviremnet"
+PINECONE_INDEX_NAME = "index name "
+>>>>>>> 681266be528be7a6a42571909bd11b6303639308
 
